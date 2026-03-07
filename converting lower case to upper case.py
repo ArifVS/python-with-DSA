ef2c980ac1faaa162767f2fc1cc43ef16b83545a
ArifVS <v.s.arifulla@gmail.com>
2026-03-07 16:41:20 +0530

    Refactor case conversion logic in Python script

--- converting lower case to upper case.py
+++ converting lower case to upper case.py
@@ -7,4 +7,15 @@ for ch in text:
     else:
         res = res + ch
 print(res)
+        OR
+
+# converting lower case to upper case
+text = "ARIF"
+res = ""
+for ch in text:
+    if 'A'<=ch<='Z':
+        res = res+chr(ord(ch)+32)
+    else:
+        res = res + ch
+print(res)
         
